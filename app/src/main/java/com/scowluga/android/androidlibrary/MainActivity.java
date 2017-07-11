84b6404c59d4e43d8d96a5df3baf274a071cb9f9
David Lu <david.scowluga@gmail.com>
2017-07-10 20:15:25 -0400

    Init

---
+++ app/src/main/java/com/scowluga/android/androidlibrary/MainActivity.java
@@ -0,0 +1,41 @@
+package com.scowluga.android.androidlibrary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    /* TODO: Learn Material Design Android Tools
+
+    General
+    - Settings
+    - Recycler View Animations
+    - Understand View Pager
+    - Animations + Transitions
+    TabLayout
+
+    - Parallax Toolbar
+    - Collapsing Toolbar
+    - The Toolbar in general (on CardViews)
+
+
+    Other Libraries
+    - AR
+    - Picasso + Bitmaps + Drawables
+    - Android Volley?
+    - JUNIT TESTING + Automated Testing?
+
+     */
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
